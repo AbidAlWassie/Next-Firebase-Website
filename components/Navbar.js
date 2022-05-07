26ba8471dae824da57d4a710602f5278a1bfdc1f
Abid Al Wassie <abidalwassie@outlook.com>
2022-05-07 14:24:44 +0600

    testing

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,13 +2,27 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="flex justify-between items-center">
+
       <div className="logo">
-        <h1>Nav List</h1>
+        <h1 className="text-[1.5rem]">Nav Logo</h1>
       </div>
-      <Link href="/" className="px-4">Home</Link>
-      <Link href="/about" className="px-4">About</Link>
-      <Link href="/services" className="px-4">Services</Link>
+
+      <ul className="nav-list m-0 p-0 flex text-[1.2rem]">
+
+        <li className="nav-link px-2">
+          <Link href="/">Home</Link>
+        </li>
+
+        <li className="nav-link px-2">
+          <Link href="/about">About</Link>
+        </li>
+
+        <li className="nav-link px-2">
+          <Link href="/services">Services</Link>
+        </li>
+      </ul>
+
     </nav>
   )
 }
