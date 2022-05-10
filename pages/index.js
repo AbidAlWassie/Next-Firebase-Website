33fcedb60b5d0ad1d23b0d80c9112501d3969e40
Abid Al Wassie <abidalwassie@outlook.com>
2022-05-10 19:37:48 +0600

    fixed navbar bug

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
+    <div className="container">
       <div className="Home">
         <h1>Homepage</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus natus omnis rem molestiae quo animi, aspernatur doloremque velit suscipit reprehenderit.</p>
